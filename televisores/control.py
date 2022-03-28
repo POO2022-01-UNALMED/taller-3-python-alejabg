fde25042037ff77b6c44a498941f31f35895cb37
Alejandra Barrientos Grisales <abarrientos@unal.edu.co>
2022-03-28 18:00:40 -0500

    Update control.py

--- televisores/control.py
+++ televisores/control.py
@@ -19,8 +19,8 @@ class Control:
     def volumenDown (self):
         self._tv.volumenDown()
 
-    def setCanal(self, canal):      
-         self._tv._canal = canal
+    def setCanal(self, canal): 
+        self._tv._canal = canal
 
     def getTv(self):
         return self._tv
