d96b0294b4f98442faebf9aeab0ed9da193253b2
Alejandra Barrientos Grisales <abarrientos@unal.edu.co>
2022-03-28 18:05:29 -0500

    Update control.py

--- televisores/control.py
+++ televisores/control.py
@@ -20,7 +20,7 @@ class Control:
         self._tv.volumenDown()
 
     def setCanal(self, canal):
-        if(self._estado == True and canal >= 1 and canal <= 120): 
+        if(self._tv._estado == True and canal >= 1 and canal <= 120): 
             self._tv._canal = canal
 
     def getTv(self):
