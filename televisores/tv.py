341b2024e4be6f30d2d1f94952bfe83718768fd1
Alejandra Barrientos Grisales <abarrientos@unal.edu.co>
2022-03-28 17:46:27 -0500

    Update tv.py

--- televisores/tv.py
+++ televisores/tv.py
@@ -2,13 +2,13 @@ class TV:
  
     _numTV = 0
 
-    def __init__(self, marca, canal, precio, estado, volumen, control):
+    def __init__(self, marca, estado):
         self._marca = marca
         self._canal = 1
         self._precio = 500
         self._estado = estado
         self._volumen = 1
-        self._control = control
+        
         TV._numTV += 1
     
     def getMarca(self):
